db0d0547fa4fe2a1cbc350f532430dfcd21b35fd
James Carman <jcarman@callibrity.com>
2024-02-08 19:13:46 -0500

    Logging thread name

--- src/main/java/sched/CustomersController.java
+++ src/main/java/sched/CustomersController.java
@@ -1,6 +1,7 @@
 package sched;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +14,14 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 class CustomersHttpController {
 
     private final CustomerRepository repository;
 
     @GetMapping("/customers")
     public List<Customer> customers() {
+        log.info("Rest controller method has been called {}", Thread.currentThread());
         return repository.findAll();
     }
 
